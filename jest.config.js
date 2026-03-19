11996c0192f1b61c195483a9055be1a3fc2af917
Nirmal188 <nirmalr5322@gmail.com>
2026-03-19 22:51:45 +0530

    fix: add jest-allure2-reporter for allure reports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    ['jest-allure2-reporter', { outputDir: 'allure-results' }]
+  ],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['tests/e2e']
